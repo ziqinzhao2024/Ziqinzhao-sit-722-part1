6aa9846ea2f3d8ae73b5e6631f9e01626601d43c
ziqinzhao2024 <s217590332@deakin.edu.au>
2024-08-03 23:27:29 +1000

    1

---
+++ app/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import settings
+
+# SQLAlchemy database engine and session
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
